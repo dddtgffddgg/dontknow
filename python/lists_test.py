a9a0067770692215eb80fd58cb08e754a5509277
still-coding <you@example.com>
2023-03-09 13:39:50 +0500

    add data file

--- python/lists_test.py
+++ python/lists_test.py
@@ -2,31 +2,7 @@ import pytest
 
 from list_node import ListNode
 from my_list import MyList
-
-SINGLE_VALUES1 = [0, 1, 2, 3, 4]
-SINGLE_VALUES2 = [1, 22, 333, 69, 420]
-DOUBLE_VALUES = [[0, 1], [1, 2], [7, 3], [69, 420]]
-TRIPLE_VALUES = [[0, 1, 2], [7, 3, 4], [69, 420, 42]]
-DIFFERENT_LISTS = [
-    [1],
-    SINGLE_VALUES1,
-    SINGLE_VALUES2,
-    *DOUBLE_VALUES,
-    *TRIPLE_VALUES,
-]
-
-DIFFERENT_LISTS_WITH_EMPTY = [[]] + DIFFERENT_LISTS
-
-
-def get_list_str(input_seq):
-    if input_seq:
-        return "(" + ") -> (".join(map(str, input_seq)) + ") -> None"
-    return "None"
-
-
-STR_CHECK_2_VALUES = [(t, get_list_str(t)) for t in DOUBLE_VALUES]
-STR_CHECK_3_VALUES = [(t, get_list_str(t)) for t in TRIPLE_VALUES]
-
+from test_data import *
 
 @pytest.fixture(params=[SINGLE_VALUES1, SINGLE_VALUES2])
 def get_5_nodes(request):
